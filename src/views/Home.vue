1d9e83bca55990388cf58d0f9c6d3cc345604bf6
Jerry <1262001568@qq.com>
2020-05-08 10:02:27 +0800

    [新增]首次提交跟随特效

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <canvas id="myCanvas">浏览器不支持</canvas>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import ball from '@/utils/Ball.js'
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      ballArray: [],
+      ctx: '',
+      canvasWidth: document.body.scrollWidth,
+      canvasHeight: document.documentElement.clientHeight,
+      colorArray: ['red', 'blue', 'green', 'purple', 'pink', 'yellow']
+    }
+  },
+  mounted() {
+    let myCanvasEle = document.getElementById('myCanvas')
+    this.ctx = myCanvasEle.getContext('2d')
+    myCanvasEle.height = this.canvasHeight
+    myCanvasEle.width = this.canvasWidth
+    myCanvasEle.style.background = 'black'
+
+    //鼠标移动事件
+    myCanvasEle.addEventListener('mousemove', e => {
+      this.ballArray.push(ball.getBall(this.ctx, e.offsetX, e.offsetY, this.colorArray[Math.floor(Math.random() * this.colorArray.length)]))
+    })
+
+    //定时器
+    setInterval(() => {
+      this.clear()
+      this.draw()
+    }, 50)
+  },
+  methods: {
+    clear() {
+      //清屏
+      this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight)
+    },
+    draw() {
+      //绘制
+      for (let i = 0; i < this.ballArray.length; i++) {
+        this.ballArray[i].draw()
+        this.ballArray[i].update()
+      }
+    }
   }
 }
 </script>
+
+<style scoped>
+
+</style>
